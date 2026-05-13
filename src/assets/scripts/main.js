efa061d3ab7c1ea16d2daa872aeb64c5dab3982d
oromeraa <oromeraa@uoc.edu>
2026-05-13 13:03:27 +0200

    inscription validation

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,8 +1,3 @@
-/**
- * Import dependencies from node_modules
- * see commented examples below
- */
-
 import "@fontsource/ubuntu/index.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
