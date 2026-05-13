521637328a1e6141f42d1ed01d0cae48440634ea
oromeraa <oromeraa@uoc.edu>
2026-05-13 10:32:57 +0200

    inscription

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -7,4 +7,14 @@ import "@fontsource/ubuntu/index.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-
+// Form validation
+const form = document.querySelector(".inscription-form");
+if (form) {
+  form.addEventListener("submit", (event) => {
+    if (!form.checkValidity()) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    form.classList.add("was-validated");
+  });
+}
